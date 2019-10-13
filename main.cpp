999455a8b1638541a1953d35986e0d3e0a65d911
dark-angel-jpg <55463775+dark-angel-jpg@users.noreply.github.com>
2019-10-13 18:39:36 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main() {
+	int a = 3,
+        b = 3.14;
+	double x = 3,  y = 3.14;
+	std::cout << "a=" <<a;
+	std::cout << "\n b="<<b;
+	std::cout << "\n x="<<x;
+	std::cout << "\n y="<<y;
+	return 0;
+}
