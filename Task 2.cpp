9b23f226e393e7f625361f4acf17386babf2528b
dark-angel-jpg <55463775+dark-angel-jpg@users.noreply.github.com>
2020-01-13 20:48:24 +0300

    Update and rename main.cpp to Task 2.cpp

--- Task 2.cpp
+++ Task 2.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 int main() {
-	int a = 3,
-        b = 3.14;
-	double x = 3,  y = 3.14;
+	int a = 3;
+        int b = 3.14;
+	double x = 3;
+	double y = 3.14;
 	std::cout << "a=" <<a;
 	std::cout << "\n b="<<b;
 	std::cout << "\n x="<<x;
